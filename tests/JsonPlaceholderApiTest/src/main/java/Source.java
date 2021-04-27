f5424bb6994c18fdb9b1b5a7a9c8ab5aa7667234
vslve <ktiiczcthddm@mail.ru>
2021-04-28 01:26:35 +0300

    JSONPlaceholderAPI test

---
+++ tests/JsonPlaceholderApiTest/src/main/java/Source.java
@@ -0,0 +1,52 @@
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.io.IOException;
+
+public class Source {
+
+    private static String getEndPoint(String url, String... params) {
+        StringBuilder endPoint = new StringBuilder(url);
+        if (params.length == 0) {
+            return endPoint.toString();
+        }
+        endPoint.append("?");
+        for (int i = 0; i < params.length; ++i) {
+            if (i - 1 % 2 == 1) {
+               endPoint.append("&");
+            }
+            endPoint.append(params[i]);
+            if (i % 2 == 0) {
+                endPoint.append("=");
+            }
+        }
+        return endPoint.toString();
+    }
+
+    /**
+     * @param url url without parameters
+     * @param params pairs param, value represents url parameters
+     *
+     * Example:
+     *
+     *         to get response body from
+     *               https://jsonplaceholder.typicode.com/posts?userId=5&title=post-title
+     *         pass to function
+     *
+     *               url = https://jsonplaceholder.typicode.com/posts
+     *               params = userId, 5, title, post-title
+     */
+    public static JSONArray getResponseBody(String url, String... params) throws IOException, JSONException {
+        assert params.length % 2 == 0;
+
+        HttpUriRequest request = new HttpGet(getEndPoint(url, params));
+        HttpResponse response = HttpClientBuilder.create().build().execute( request );
+        String jsonFromResponse = EntityUtils.toString(response.getEntity());
+        return new JSONArray(jsonFromResponse);
+    }
+}
---
+++ tests/JsonPlaceholderApiTest/src/test/java/UserIdWithTitleTest.java
@@ -0,0 +1,56 @@
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+public class UserIdWithTitleTest {
+    final String baseUrl  = "https://jsonplaceholder.typicode.com/posts";
+    final String userIdParam = "userId";
+    final String titleParam = "title";
+
+    final List<Integer> correctUserIds = List.of(1, 10);
+    final Map<Integer, List<String>> correctSpecificUserTitles = Map.ofEntries(
+            Map.entry(1, List.of("qui est esse", "magnam facilis autem")),
+            Map.entry(10, List.of("laboriosam dolor voluptates", "beatae soluta recusandae"))
+    );
+
+
+    @Test
+    public void test() throws JSONException, IOException {
+        for (int userId : correctUserIds) {
+            for (String title : correctSpecificUserTitles.get(userId)) {
+                JSONArray responseBody = Source.getResponseBody(
+                        baseUrl,
+                        userIdParam,
+                        String.valueOf(userId),
+                        titleParam,
+                        title.replace(" ", "%20")
+                );
+                testPostsCount(responseBody, 1);
+
+                JSONObject post = responseBody.getJSONObject(0);
+                testPostUserId(post, userId);
+                testPostTitle(post, title);
+            }
+        }
+    }
+
+    public void testPostsCount(JSONArray responseBody, int expectedCount) {
+        Assertions.assertEquals(expectedCount, responseBody.length());
+    }
+
+    public void  testPostUserId(JSONObject post, int expectedUserId) throws JSONException {
+        Assertions.assertEquals(expectedUserId, post.getInt("userId"));
+    }
+
+    public void testPostTitle(JSONObject post, String expectedTitle) throws JSONException {
+        Assertions.assertEquals(expectedTitle, post.getString("title"));
+    }
+
+
+
+}
